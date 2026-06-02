d08956118f57cf79380eda0696a2f887650de8ae
Shengfan <csf990123@gmail.com>
2026-06-02 12:34:26 -0700

    final cleanup on edited launch file

--- isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense_online.launch.py
+++ isaac_ros_visual_slam/launch/isaac_ros_visual_slam_realsense_online.launch.py
@@ -75,8 +75,6 @@ def generate_launch_description():
             'gyro_fps': gyro_fps,
             'accel_fps': accel_fps,
             'unite_imu_method': 2,
-            # 'camera_namespace': f'{BASE_NAMESPACE}/{CAMERA_NAME}',
-            # 'camera_name': CAMERA_NAME,
         }],
     )
 
